c66401672a0da1258df9d06241633bddbf2b9e93
Sai628 <sai100628@gmail.com>
2018-05-11 14:35:36 +0800

    refactor code

--- app/src/main/java/com/sai628/moviejie/activity/base/BaseActivity.java
+++ app/src/main/java/com/sai628/moviejie/activity/base/BaseActivity.java
@@ -2,12 +2,10 @@ package com.sai628.moviejie.activity.base;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+
+import com.sai628.moviejie.utils.DisplayUtil;
 
 
 /**
@@ -18,16 +16,7 @@ public class BaseActivity extends Activity
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.WHITE);
-        }
+        DisplayUtil.setupStatusBar(this, Color.WHITE);
         super.onCreate(savedInstanceState);
     }
 
--- app/src/main/java/com/sai628/moviejie/utils/DisplayUtil.java
+++ app/src/main/java/com/sai628/moviejie/utils/DisplayUtil.java
@@ -3,13 +3,17 @@ package com.sai628.moviejie.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Rect;
+import android.os.Build;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 /**
  * @author Sai
  * @ClassName: DisplayUtil
- * @Description: 显示尺寸单位转换工具类
+ * @Description: 显示工具类
  * @date 2013-9-27 下午3:41:26
  */
 public class DisplayUtil
@@ -123,5 +127,25 @@ public class DisplayUtil
     public static int getViewHeightWithoutStatusBarPixels(Activity activity)
     {
         return getDeviceHeightPixels(activity) - getStatusBarHeight(activity);
+    }
+
+
+    /**
+     * 设置状态栏风格
+     * @param activity
+     * @param color 状态栏颜色
+     */
+    public static void setupStatusBar(Activity activity, int color)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            Window window = activity.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(color);
+        }
     }
 }
